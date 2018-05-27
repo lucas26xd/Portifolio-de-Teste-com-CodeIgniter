a392ac6bb4f38888d57153f9df6e71500b8b4bf1
Hugo Sousa <hugosousa111@gmail.com>
2018-05-27 01:19:47 -0300

    Centralização footer

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,19 +1,21 @@
 </main><!-- /.container -->
 
 <br><br><br><br>
+<div class="container-fluid">
+  <footer>
+    Atividade 3 de Tecnologias Web - Prof. Me. Joseph Soares Alcântara
+    <address>
+      <img class="imgs_peq" src="<?=base_url('static/img/mail.png')?>" alt="E-mail:">
+      <a href="mailto:atendimento@PizzaDelicia.com">atendimento@PizzaDelicia.com</a>
+      <img class="imgs_peq" src="<?=base_url('static/img/wpp.png')?>" alt="Wpp:">
+      <a href="https://api.whatsapp.com/send?phone=88996695833" target="_blank">(88) 9-9669-5833</a>
+    </address>
+    <p>
+      <small>Trabalho 3&copy; <?=date('Y')?> - Todos os direitos reservados.</small>
+    </p>
+  </footer>  
+</div>
 
-<footer>
-  Atividade 3 de Tecnologias Web - Prof. Me. Joseph Soares Alcântara
-  <address>
-    <img class="imgs_peq" src="<?=base_url('static/img/mail.png')?>" alt="E-mail:">
-    <a href="mailto:atendimento@PizzaDelicia.com">atendimento@PizzaDelicia.com</a>
-    <img class="imgs_peq" src="<?=base_url('static/img/wpp.png')?>" alt="Wpp:">
-    <a href="https://api.whatsapp.com/send?phone=88996695833" target="_blank">(88) 9-9669-5833</a>
-  </address>
-  <p>
-    <small>Trabalho 3&copy; <?=date('Y')?> - Todos os direitos reservados.</small>
-  </p>
-</footer>
 
 <!-- jQuery, Popper.js, Bootstrap JS, Custom Script -->
 <script type="text/javascript" src="<?=base_url('static/js/jquery-3.3.1.min.js')?>"></script>
