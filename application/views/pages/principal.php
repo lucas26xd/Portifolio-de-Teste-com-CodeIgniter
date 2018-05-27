268ea3c248a09fa3ee58c9a90cdcc8f5bec639d3
angelamf <angelam0952@gmail.com>
2018-05-26 23:52:26 -0300

    Alterando conteúdo da principal

--- application/views/pages/principal.php
+++ application/views/pages/principal.php
@@ -1,1 +1,3 @@
-      <h1 class="al" id="al">Ângela, Lucas, Tadeu, Kamila e Hugo</h1>
+<h2>Olá! Seja Bem-Vindo!</h2>
+<h3>Tenha acesso ao perfil da nossa equipe acessando o item <i>sobre</i> do menu!</h3>
+<h1 class="al" id="al">Ângela, Lucas, Tadeu, Kamila e Hugo</h1>
