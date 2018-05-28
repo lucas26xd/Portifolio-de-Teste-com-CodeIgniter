ca08cbfe88f01c0b69749ffe3c11b178c1ce46a4
Hugo Sousa <hugosousa111@gmail.com>
2018-05-28 14:45:10 -0300

    sobre estatico

--- application/views/pages/sobre.php
+++ application/views/pages/sobre.php
@@ -1,5 +1,12 @@
-<div class="container-fluid">
-  <img class="img-fluid" src="<?=base_url('static/img/hugo.png')?>">
-  <h1>Hugo Sousa</h1>
-
-</div>
+<div id = "teste" class="container-fluid">
+	  <img class="img-fluid" src="<?=base_url('static/imagens/calabresa1.png')?>">
+	  <h1>Hugo Sousa</h1>
+	  <h1>Telefone</h1>
+	  <h1>email</h1>
+	  <h1>Texto Texto Texto Texto Texto  Texto Texto
+	    Texto Texto Texto Texto Texto Texto Texto Texto
+	    Texto Texto Texto Texto Texto Texto Texto Texto
+	    Texto Texto Texto Texto Texto Texto Texto Texto
+	    Texto Texto Texto Texto Texto Texto Texto Texto
+	    Texto Texto Texto Texto Texto Texto Texto Texto </h1>
+	</div>
