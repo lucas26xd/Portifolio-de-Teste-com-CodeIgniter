3d26f5066c19a8237b1bedd11cc5488dc8a11b85
Lucas Santos <lucas26xd@gmail.com>
2018-05-29 12:40:56 -0300

    Teste pc novo

--- static/js/code.js
+++ static/js/code.js
@@ -1,4 +1,4 @@
-function trocaImagem() {
+function trocaImagem() {//troca posição das imagens
   var fotos = $("div.fotos").children();
   for (var i = 1; i < fotos.length; i++) {
     fotos[0].before(fotos[i]);
